47c65c61ae5a1e8dcb2649663c9bbc14d16b9773
mrust <m.rustam1305@gmail.com>
2023-03-18 19:01:05 +0500

    Base pages

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 
+@app.route('/login')
+def login():
+    return render_template('login.jinja', title='Login')
+
+
+@app.route('/user/<user_login>')
+def user_page(user_login):
+    return render_template('user_page.jinja', title='Main Page')
+
+
+@app.route('/messanger')
+def messanger():
+    return render_template('messanger.jinja', title='Messanger')
+
+
 def main():
     db_session.global_init('db/db.db')
     app.run(port=8080, host='127.0.0.1', debug=True)
